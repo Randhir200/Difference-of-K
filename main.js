8847f13be5c2097288545740f95422195510a0a0
Randhir <mail.randhirkr@gmail.com>
2022-04-21 20:43:22 +0530

    Create main.js
    first-commit

---
+++ main.js
@@ -0,0 +1,24 @@
+var [n, k] = [5,3];  //1
+var arr = [1,1,3,3,5] // 1
+
+// 5-1 = 4, 4>3{right--},4-1
+// 5-1 = 4,4>3(left++) 5-2
+var left=0;
+var right = n-1;
+var flag = false;
+while(left<n && right>=0){ //n
+     if(arr[right]-arr[left]==k){
+         console.log("Yes");
+         flag=true;
+         break;
+     }else if(arr[right]-arr[left]>k){
+         right--;
+     }else{
+         left++;
+     }
+}
+
+if(!flag){
+    console.log("No");
+}
+   
